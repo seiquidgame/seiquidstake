b37f8c83b64efb326a956eb5e4f1e2e23d55634b
SeiQuidXYZ <seiquidgame@gmail.com>
2025-02-05 22:23:16 +0800

    Delete components/src directory

--- components/src/app/chain.ts
+++
@@ -1,8 +0,0 @@
-import { defineChain } from "thirdweb/chains";
-//import { Sei } from "thirdweb/chains";
-
-export const chain = defineChain({
-    id: 1329,
-    rpc: "https://evm-rpc.sei-apis.com"
-    //rpc: "https://rpc.sei-apis.com/"
-});
